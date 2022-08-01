cea59093a62695e6127f706540669ed43330842a
Vid Žohar <87440406+VidZohar@users.noreply.github.com>
2022-08-01 10:18:13 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+
+# API Keys for Binance Testnet:
+BINANCE_API_KEY = '9t58Jb7Q6wKaaTOzMgySMouCspG5kalT49fUSrHo62XUNtiis6A2qhzClarPyzFA'
+BINANCE_SECRET_KEY = '****************************************************************'
